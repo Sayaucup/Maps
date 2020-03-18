5f94daed09ca35bc78870b0f052b60f3647dc77b
ucupyh <hadiyus321@gmail.com>
2020-03-18 14:52:43 +0700

    Add marker origin and destination

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Alert} from 'react-native';
-import MapView, {
-  PROVIDER_GOOGLE
-} from 'react-native-maps'; 
+import MapView, {PROVIDER_GOOGLE, Marker,Polygon} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import MapViewDirections from 'react-native-maps-directions';
 
 console.disableYellowBox = true;
+
+const coordinates = [
+  {
+    latitude: -7.9964468,
+    longitude: 110.2953939,
+  },
+  {
+    latitude: -7.978077,
+    longitude: 110.316476,
+  },
+];
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBs0MBQVuA7QeWRQFWy3RRaIVrkCeWOcZA';
 
 class App extends Component {
   constructor(props) {
@@ -19,9 +30,11 @@ class App extends Component {
     Geolocation.getCurrentPosition(
       position => {
         console.log(JSON.stringify(position));
+        const {latitude, longitude} = position.coords;
+
         let initialPosition = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+          latitude,
+          longitude,
           latitudeDelta: 0.09,
           longitudeDelta: 0.035,
         };
@@ -40,7 +53,21 @@ class App extends Component {
           showsUserLocation={true}
           ref={map => (this._map = map)}
           initialRegion={this.state.initialPosition}
-          provider={PROVIDER_GOOGLE}></MapView>
+          provider={PROVIDER_GOOGLE}>
+          <MapViewDirections
+            origin={coordinates[0]}
+            destination={coordinates[1]}
+            waypoints={coordinates}
+            region="en"
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={5}
+            strokeColor="red"
+          />
+          {/* <Polygon
+            coordinates={coordinates}/> */}
+          <Marker coordinate={coordinates[0]} title="qew" />
+          <Marker coordinate={coordinates[1]} title="das" />
+        </MapView>
       </View>
     );
   }
